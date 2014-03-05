d1705ff527d6fa2508f7962961903e2058a54081
raffdoc <raffdoc@yahoo.com>
2014-03-05 18:02:47 +0100

    Table is added to the main text.

--- code/data_prep.R
+++ code/data_prep.R
@@ -1,5 +1,5 @@
 # read data 
-outcome <- read.csv("data/outcome-of-care-measures.csv", colClasses = "character")
+outcome <- read.csv("data/outcome-of-care-measures.csv", colClasses = "character", na.string="Not Available")
 #
 outcome[,11] <- as.numeric(outcome[,11])
 outcome[,17] <- as.numeric(outcome[,17])
@@ -10,3 +10,32 @@ outcome.hospital <- merge(outcome, hospital, by = "Provider.Number")
 death <- as.numeric(outcome.hospital[, 11])  ## Heart attack outcome
 npatient <- as.numeric(outcome.hospital[, 15])
 owner <- factor(outcome.hospital$Hospital.Ownership)
+# example from melanoma data
+library(boot)
+
+# Set time to years instead of days
+melanoma$time_years <- 
+  melanoma$time / 365.25
+
+# Factor the basic variables that 
+# we're interested in
+melanoma$status <- 
+  factor(melanoma$status, 
+         levels=c(2,1,3),
+         labels=c("Alive", # Reference
+                  "Melanoma death", 
+                  "Non-melanoma death"))
+melanoma$sex <- 
+  factor(melanoma$sex,
+         labels=c("Male", 
+                  "Female"))
+
+melanoma$ulcer <- 
+  factor(melanoma$ulcer,
+         labels=c("Present", 
+                  "Absent"))
+# Setup needed for the rms coxph wrapper
+ddist <- datadist(melanoma)
+options(datadist = "ddist")
+fit <- cph(Surv(melanoma$time, melanoma$status=="Melanoma death") ~
+             sex + age + thickness + ulcer, data=melanoma)
